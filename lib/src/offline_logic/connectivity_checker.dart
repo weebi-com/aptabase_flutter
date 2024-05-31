a4e86b016dad6bd8eb732dd625f3495f4fb1ad71
pierre <pierre@weebi.com>
2024-05-31 16:48:16 +0200

    bla bla

--- lib/src/offline_logic/connectivity_checker.dart
+++ lib/src/offline_logic/connectivity_checker.dart
@@ -7,17 +7,18 @@ import 'package:flutter/services.dart';
 
 Future<bool> get isConnected async {
   try {
-    final result = await Connectivity().checkConnectivity();
+    final results = await Connectivity().checkConnectivity();
     if (kIsWeb) {
-      if (result == ConnectivityResult.wifi ||
-          result == ConnectivityResult.ethernet ||
-          result == ConnectivityResult.bluetooth) {
+      if (results.any((e) =>
+          e == ConnectivityResult.wifi ||
+          e == ConnectivityResult.ethernet ||
+          e == ConnectivityResult.bluetooth)) {
         return true;
       } else {
         return false;
       }
     } else {
-      return await isInternetAvailable(result);
+      return await isInternetAvailable(results);
     }
   } on PlatformException catch (e) {
     developer.log(e.toString());
--- lib/src/offline_logic/connectivity_checker_webless.dart
+++ lib/src/offline_logic/connectivity_checker_webless.dart
@@ -3,8 +3,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 // Package imports:
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
-Future<bool> isInternetAvailable(ConnectivityResult result) async {
-  if (result == ConnectivityResult.none) {
+Future<bool> isInternetAvailable(List<ConnectivityResult> results) async {
+  if (results.first == ConnectivityResult.none) {
     return false;
   } else {
     return await InternetConnectionChecker().hasConnection;
